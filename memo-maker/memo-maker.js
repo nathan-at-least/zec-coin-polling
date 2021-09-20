ad4196843a326c42c455b7a938e5e5d3e86d8e17
Nate Wilcox <nathan+dev@electriccoin.co>
2021-09-20 12:06:01 -0700

    Introduce ballot JSON schema validation.

--- memo-maker/memo-maker.js
+++ memo-maker/memo-maker.js
@@ -11,10 +11,11 @@ async function handle_file(ev) {
   console.log('Handling file: ' + file.name);
   const text = await file.text();
   const doc = JSON.parse(text);
-  render_ballot_json(doc);
+  validate_ballot_json(doc);
+  render_ballot(doc);
 }
 
-function render_ballot_json(doc) {
+function render_ballot(doc) {
   const divballot = document.getElementById('div-ballot');
   append_children(
     divballot,
@@ -30,12 +31,12 @@ function render_ballot_json(doc) {
 function mk_elem(tag, attrs, children) {
   const elem = document.createElement(tag);
   for_each_kv(
-    (attrs === undefined) ? {} : attrs,
+    default_value(attrs, {}),
     (k, v) => {
       elem.setAttribute(k, v)
     }
   );
-  append_children(elem, (children === undefined) ? [] : children);
+  append_children(elem, default_value(children, []))
   return elem;
 }
 
@@ -58,7 +59,6 @@ function append_children(elem, children) {
 
     } else {
       set_error('Invalid Child Entry: ' + entry);
-      throw entry;
     }
   }
 
@@ -71,6 +71,7 @@ function set_error(msg) {
   const diverror = document.getElementById('div-error');
   diverror.textContent = errmsg;
   diverror.hidden = false;
+  throw errmsg;
 }
 
 function hide_elems(ids) {
@@ -80,5 +81,121 @@ function hide_elems(ids) {
 function for_each_kv(obj, cb) {
   for (const k in obj) {
     cb(k, obj[k]);
+  }
+}
+
+function validate_ballot_json(doc) {
+  function predicate(p, desc) {
+    p.predicate_description = desc;
+    return p;
+  }
+
+  function and(a, b) {
+    return predicate(
+      (v) => a(v) && b(v),
+      a.predicate_description + ' and ' + b.predicate_description,
+    );
+  }
+
+  function or(a, b) {
+    return predicate(
+      (v) => a(v) || b(v),
+      a.predicate_description + ' or ' + b.predicate_description,
+    );
+  }
+
+  const is_type = (t) => predicate((v) => typeof v == t, 'not a ' + t);
+
+  const SCHEMA = {
+    "zec-coin-polling-ballet": predicate(
+      (v) => v == 'v1',
+      'unknown version',
+    ),
+    "title": is_type('string'),
+    "cut-off-height": and(
+      is_type('number'),
+      predicate((v) => Number.isInteger(v), 'not an integer'),
+    ),
+    "vote-reception-address": is_type('string'),
+    "vote-reception-viewing-key": is_type('string'),
+    "poll-questions": [
+      {
+        "question": is_type('string'),
+        "other-prompt": or(
+          predicate((v) => v === null, 'null'),
+          is_type('string'),
+        ),
+        "fixed-responses": [ is_type('string') ],
+      }
+    ]
+  };
+
+  function validate_schema(thing, schema, path) {
+    if (typeof schema == 'function') {
+      validate_schema_predicate(thing, schema, path);
+
+    } else if (Array.isArray(schema)) {
+      validate_schema_array(thing, schema, path);
+
+    } else if (typeof schema == 'object') {
+      validate_schema_object(thing, schema, path);
+
+    } else {
+      assert(false, 'unknown schema type: ' + schema);
+    }
+  }
+
+  function validate_schema_predicate(thing, pred, path) {
+    if (!pred(thing)) {
+      const desc = pred.predicate_description;
+      assert(desc !== undefined, 'schema.predicate_description undefined');
+      set_error('' + path + ' is ' + desc + ': ' + thing);
+    }
+  }
+
+  function validate_schema_array(thing, schema, path) {
+    assert(schema.length == 1);
+    const elemschema = schema[0];
+    validate_schema(thing, predicate(Array.isArray, 'not an array'));
+    thing.forEach((x, i) => validate_schema(x, elemschema, path + '[' + i + ']'));
+  }
+
+  function validate_schema_object(thing, schema, path) {
+    const unprocessed = {};
+    Object.getOwnPropertyNames(thing).forEach((k) => {
+      unprocessed[k] = null;
+    });
+
+    const parsed = {};
+    for (const field in schema) {
+      validate_schema(
+        thing[field],
+        schema[field],
+        path + '.' + field,
+      );
+      delete unprocessed[field];
+    }
+
+    // Anything left on thing is unknown:
+    validate_schema(
+      Object.getOwnPropertyNames(unprocessed),
+      predicate(
+        (a) => a.length == 0,
+        'unknown fields',
+      ),
+      path,
+    );
+  }
+
+  validate_schema(doc, SCHEMA, '<Ballot JSON>');
+}
+
+function default_value(v, def) {
+  return (v === undefined) ? def : v;
+}
+
+function assert(cond, msg) {
+  if (!cond) {
+    set_error('Internal assertion failure: ' + msg);
   }
 }
